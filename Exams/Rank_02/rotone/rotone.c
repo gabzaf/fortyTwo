d49d38bcf8520247696d9f65328e0a43c9bec981
gab Deb <gabriel.affonsoo@gmail.com>
2024-07-31 13:58:24 +0100

    studying

--- Exams/Rank_02/rotone/rotone.c
+++ Exams/Rank_02/rotone/rotone.c
@@ -3,32 +3,28 @@
 int	main(int argc, char **argv)
 {
 	int	i;
-	char	p;
+	char	c;
 
 	if (argc == 2)
 	{
 		i = 0;
 		while (argv[1][i])
 		{
-			if (argv[1][i] >= 65 && argv[1][i] <= 90)
+			if (argv[1][i] >= 'A' && argv[1][i] <= 'Z')
 			{
-				if (argv[1][i] == 90)
-					write(1, "A", 1);
+				if (argv[1][i] == 'Z')
+					c = 'A';
 				else
-				{
-					p = argv[1][i] + 1;
-					write(1, &p, 1);
-				}
+					c = argv[1][i] + 1;
+				write(1, &c, 1);
 			}
-			else if (argv[1][i] >= 97 && argv[1][i] <= 122)
+			else if (argv[1][i] >= 'a' && argv[1][i] <= 'z')
 			{
-				if (argv[1][i] == 122)
-					write(1, "a", 1);
+				if (argv[1][i] == 'z')
+					c = 'a';
 				else
-				{
-					p = argv[1][i] + 1;
-					write(1, &p, 1);
-				}
+					c = argv[1][i] + 1;
+				write(1, &c, 1);
 			}
 			else
 				write(1, &argv[1][i], 1);
