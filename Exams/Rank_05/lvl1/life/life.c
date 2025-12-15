e1a7e8b8cb10010f20b95cf1312ac253e15806f8
rei <gabriel.affonsoo@gmail.com>
2025-12-15 12:41:25 +0000

    studying

--- Exams/Rank_05/lvl1/life/life.c
+++ Exams/Rank_05/lvl1/life/life.c
@@ -2,49 +2,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct	pen
+typedef struct pen
 {
 	int	x;
 	int	y;
 	int	isdown;
 } pen;
 
-void	print_board(char **b, int w, int h)
-{
-	for (int i = 0; i < h; i++)
-	{
-		for (int j = 0; j < w; j++)
-			putchar(b[i][j]);
-		putchar('\n');
-	}
-}
-
 char	**create_board(int w, int h)
 {
-	char	**board = (char **)malloc(sizeof(char *) * h);
-	for (int i = 0; i < h; i++)
+	char	**b = (char **)malloc(sizeof(char *) * h);
+	for (int j = 0; j < h; j++)
 	{
-		board[i] = (char *)malloc(sizeof(char) * w);
-		for (int j = 0; j < w; j++)
-		{
-			board[i][j] = ' ';
-		}
+		b[j] = (char *)malloc(sizeof(char) * w);
+		for (int i = 0; i < w; i++)
+			b[j][i] = ' ';
 	}
-	return (board);
-}
-
-int	count(char **b, int w, int h, int x, int y)
-{
-	int	c = 0;
-	for (int i = -1; i <= 1; i++)
-	{
-		for (int j = -1; j <= 1; j++)
-		{
-			if (!(i == 0 && j == 0) && i + y >= 0 && i + y < h && j + x >= 0 && j + x < w && (b[i + y][j + x] == '0'))
-				c++;
-		}
-	}
-	return (c);
+	return (b);
 }
 
 void	free_board(char **b, int h)
@@ -54,35 +28,56 @@ void	free_board(char **b, int h)
 	free(b);
 }
 
-void	iter_game(char ** board, int w, int h)
+int	count_n(char **b, int w, int h, int x, int y)
+{
+	int c = 0;
+	for (int j = -1; j <= 1; j++)
+	{
+		for (int i = -1; i <= 1; i++)
+		{
+			if (!(j == 0 && i == 0) && j + y >= 0 && j + y < h && i + x >= 0 && i + x < w && (b[j + y][i + x] == '0'))
+				c++;
+		}
+	}
+	return (c);
+}
+
+void	iter_game(char **b, int w, int h)
 {
 	char	**clone = create_board(w, h);
 	for (int y = 0; y < h; y++)
 	{
 		for (int x = 0; x < w; x++)
 		{
-			int c = count(board, w, h, x, y);
-			if ((board[y][x] == '0' && (c == 2 || c == 3)) || (board[y][x] != '0' && c == 3))
+			int	c = count_n(b, w, h, x, y);
+			if ((b[y][x] == '0' && (c == 2 || c == 3)) || (b[y][x] != '0' && c == 3))
 				clone[y][x] = '0';
 			else
 				clone[y][x] = ' ';
 		}
 	}
-	for (int i = 0; i < h; i++)
+	for (int j = 0; j < h; j++)
 	{
-		for (int j = 0; j < w; j++)
-		{
-			board[i][j] = clone[i][j];
-		}
+		for (int i = 0; i < w; i++)
+			b[j][i] = clone[j][i];
 	}
 	free_board(clone, h);
+}
+
+void	print_board(char **b, int w, int h)
+{
+	for (int j = 0; j < h; j++)
+	{
+		for (int i = 0; i < w; i++)
+			putchar(b[j][i]);
+		putchar('\n');
+	}
 }
 
 int	main(int ac, char **av)
 {
 	if (ac != 4)
 		return (1);
-	
 	int	w = atoi(av[1]), h = atoi(av[2]), iter = atoi(av[3]);
 	pen	p = {0, 0, 0};
 	int	c = 0;
@@ -94,10 +89,10 @@ int	main(int ac, char **av)
 			p.isdown = !p.isdown;
 		if (c == 'w' && p.y > 0)
 			p.y--;
-		if (c == 's' && p.y < h - 1)
-			p.y++;
 		if (c == 'a' && p.x > 0)
 			p.x--;
+		if (c == 's' && p.y < h - 1)
+			p.y++;
 		if (c == 'd' && p.x < w - 1)
 			p.x++;
 		if (p.isdown)
