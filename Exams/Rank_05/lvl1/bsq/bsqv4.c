7c437ea534ebf878af5d9e5a6c2577147954da4f
Gabriel Amado Xavier Oliveira Affonso <gabriel.affonsoo@gmail.com>
2026-01-09 11:31:00 +0000

    More maps

---
+++ Exams/Rank_05/lvl1/bsq/bsqv4.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct s_bsq
+{
+	int	lines;
+	int	width;
+	char	empt;
+	char	obs;
+	char	full;
+	char	**map;
+} t_bsq;
+
+#define ERROR "Error: invalid map \n"
+
+FILE	*open_input(char *av)
+{
+	FILE	*file = av ? fopen(av, "r") : stdin;
+	if (!file)
+		fputs(ERROR, stdout), exit(1);
+	return (file);
+}
+
+int	ft_strlen(char *str)
+{
+	int	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+void	close_on_error(t_bsq *bsq, FILE *file, char *av, int i, char *line)
+{
+	if (av)
+		fclose(file);
+	if (line)
+		free(line);
+	fputs(ERROR, stdout);
+	if (i >= 0)
+		free_map(bsq, i);
+	exit(1);
+}
+
+void	parse_header(t_bsq *bsq, FILE *file, char *av)
+{
+	if (fscanf(file, "%d%c%c%c\n", &bsq->lines, &bsq->empt, &bsq->obs, &bsq->full) != 4)
+		close_on_error(bsq, file, av, -1, NULL);
+	if (bsq->lines <= 0 || bsq->empt == bsq->obs || bsq->empt == bsq->full || bsq->obs == bsq->full)
+	       close_on_error(bsq, file, av, -1, NULL);	
+}
+
+void	allocate_header(t_bsq, FILE *file, char *av)
+{
+	bsq->map = (char **)malloc(sizeof(char *) * bsq->lines);
+	if (!bsq->map)
+		close_on_error(bsq, file, av, -1, NULL);
+}
+
+char	*read_line(t_bsq, FILE *file, char *av, int i)
+{
+	char	*line = NULL;
+	size_t	len = 0;
+	if (getline(&line, &len, file) == -1)
+		close_on_error(bsq, file, av, i, line);
+	int	new_len = ft_strlen(line);
+	if (new_len > 0 && line[new_len - 1] == '\n')
+		line[new_len - 1] = '\0';
+	else if (i < bsq->lines - 1 && line[new_len - 1] != '\n')
+		close_on_error(bsq, file, av, i, line);
+	return (line);
+}
+
+void	parse_bsq(t_bsq *bsq, char *av)
+{
+	FILE	*file = open_input(av);
+	parse_header(bsq, file, av);
+	allocate_header(bsq, file, av);
+
+	for (int i = 0; i < bsq->lines; i++)
+	{
+		char	*line = read_line(bsq, file, av, i);
+		int	len = ft_strlen(line);
+		if (i == 0)
+			bsq->width = len;
+		else if (i < bsq->lines - 1 && bsq->width != len)
+			close_on_error(bsq, file, av, i, line);
+		bsq->map[i] = line;
+	}
+	if (av)
+		fclose(file);
+	check_map(bsq);
+}
+
+
+
+int	main(int ac, char **av)
+{
+	t_bsq	bsq;
+
+	if (ac == 1)
+		parse_bsq(&bsq, NULL);
+	else if (ac == 2)
+		parse_bsq(&bsq, av[1]);
+	solve_bsq(&bsq);
+	print_map(&bsq);
+	free_map(&bsq, bsq.lines);
+	return (0);
+}
